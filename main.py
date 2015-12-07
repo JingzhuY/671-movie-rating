eae93626d855d2fa46ced07285923372ba4beea5
JingzhuY <yancarol@umich.edu>
2015-12-07 11:50:41 +0800

    framework

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+1. features for user-movie pair(Linear Regression): [age, occupation, genre, gender, (ug*mg)/ng,
+average rating given by u, average rating for movie m, 1]
+
+''' 
+
+
+
+# input:  a list l
+# output: a feature matrix object ready for training
+def preprocess():
+	result=[]
+	return result
+
+def main():
+	print ''
+
+if __name__ == '__main__':
+    main()
