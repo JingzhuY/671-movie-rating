eb4b988e0cf4b71644f7775300caf7059ff0a39b
Mingjuen <mingjuen@umich.edu>
2015-12-08 21:05:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,20 @@ unset($_SESSION['name']);
 $_SESSSION['data'] = array();
 
 $salt = 'XyZzy12*_';
-$stored_hash = '9e35cfbf575d753d2fcd09db522bf5dc';	// pw is si6712015
+$stored_hash = '64f4fba5160dd3a5be63f3ec3911359f';	// pw is si6712015
 
 // error checking: to check whether user & pass are typed in 
 if( isset($_POST['who']) && isset($_POST['pass'])){
 	// if user or pass info is missing, error msg 
 	if( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1) {
-		//$failure = "User name and password are required";	 
+		//$failure = "User ID and password are required";	 
 		//Instead, redirect to the current page 
 		header("Location: index.php");
 		$_SESSION['error'] = "User ID and password are required";
 		exit();
 	}
 	// if user and pass are not blank, check for the password by comparing to the stored hash. 
-	elseif (intval($_POST['who']) > 1 &&  intval($_POST['who']) <= 6040) {
+	elseif ((int)$_POST['who'] < 1 && (int)$_POST['who'] > 6041) {
 		header("Location: index.php");
 		$_SESSION['error'] = "User ID doesn't exist. Please check again.";
 		exit();
@@ -71,5 +71,8 @@ if( isset($_POST['who']) && isset($_POST['pass'])){
 		For a password hint, view source and find a password hint in the HTML comments.
 		<!-- Hint: The password is the course number + year. -->
 	</p>
+	<!-- <p>
+	Attempt to <a href = "data.php">Edit Address Book</a> without logging in. 
+	</p>  -->
 </body>
 </html>
