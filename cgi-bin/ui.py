ff78c78fed44304e8d590f1dd99d256f8bd34d16
JingzhuY <yancarol@umich.edu>
2015-12-10 14:37:43 +0800

    dense data

--- cgi-bin/ui.py
+++ cgi-bin/ui.py
@@ -137,10 +137,10 @@ print '\r\n'
 
 recommended_movies = predictRatings(userid, user_movie, 15)
 print '<table>'
-print '<tr><td><b>Title</b></td><td><b>Genre</b></td><td><b>rating</b></td></tr>'
+print '<tr><td><b>Title</b></td><td><b>Genre</b></td>'
 for (r, m) in recommended_movies:
 	print '<tr>'
-	print '<td>'+movies[m]['name'] + '</td><td>' + '/ '.join(movies[m]['genre']) + '</td><td>'+str(r)+'</td>'
+	print '<td>'+movies[m]['name'] + '</td><td>' + '/ '.join(movies[m]['genre']) + '</td>'
 	print '</tr>'
 print '</table></p>'
 
